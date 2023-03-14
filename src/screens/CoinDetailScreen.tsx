a40a73905ff894b26f770e5e0c5f58aec10b404b
Exabiier <102698808+Exabiier@users.noreply.github.com>
2023-03-13 23:25:39 -0700

    Refractored the Header information in HeaderInfo.tsx

--- src/screens/CoinDetailScreen.tsx
+++ src/screens/CoinDetailScreen.tsx
@@ -1,8 +1,10 @@
 import { View, Text, TouchableOpacity, Image} from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { Ionicons, EvilIcons } from '@expo/vector-icons'
 import CoinDetailHeader from '../components/CoinDetailScreen/CoinDetailHeader'
+import { AntDesign } from '@expo/vector-icons'
+import HeaderInfo from '../components/CoinDetailScreen/HeaderInfo'
 
 
 type Props ={
@@ -17,16 +19,27 @@ const CoinDetailScreen = () => {
     image: { small },
     name,
     id,
-    market_data: {market_cap_rank, current_price},
+    market_data: { market_cap_rank, current_price, price_change_percentage_24h },
  } = cryptoCurrencyDataTyped
+
+  const [color, setColor] = useState<boolean>();
+
+  useEffect(() =>{
+    if(price_change_percentage_24h <= 0 ) {
+      setColor(true)
+    } else {
+      setColor(false)
+    }
+  }, [setColor, price_change_percentage_24h])
+
+  const twPercentage = "p-[5px] py-2 rounded-lg flex-row items-center space-x-1"
 
   return (
     <View className="flex-1 bg-[#121212] pt-[50px] px-1">
       <CoinDetailHeader imgURL={small} name={name} marketCap={market_cap_rank} />
-      <View className='p-[15px]'>
-        <Text className='text-white text-[15px]'>{name}</Text>
-        <Text className='text-white text-[30px] font-semibold' style={{letterSpacing:1}}>${current_price.usd}</Text>
-      </View>
+      
+      <HeaderInfo name={name} tailWindClass={twPercentage} currentPrice={current_price.usd} PriceChange={price_change_percentage_24h} color={color} />
+      
       
 
     </View>
