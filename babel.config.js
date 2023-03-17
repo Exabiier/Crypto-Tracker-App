ab061fa5cbdce26f286fa41d11b3bf36e400e95e
Exabiier <102698808+Exabiier@users.noreply.github.com>
2023-03-17 16:00:51 -0700

    Basic app i complete. Will pause until chart solution is complete.

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo', '@babel/preset-typescript'],
-    plugins: ["nativewind/babel"]
+    plugins: ["nativewind/babel", 'react-native-reanimated/plugin']
   };
 };
